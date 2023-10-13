b958b65c26387fbc75c6bcd5dce8d20f9a034e7b
BarbaraBravoRedondo <barbarabravoredondo@gmail.com>
2023-10-13 11:27:16 +0200

    random word api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,27 @@
 //imports depencencies,images,styles*//
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 // import  from '../images/react.svg'
 // import  from
 import '../styles/App.scss';
 
 function App() {
-  const [numberOfErrors, setNumberOfErrors] = useState(0);
+  // const [numberOfErrors, setNumberOfErrors] = useState(0);
   const [lastLetter, setLastLetter] = useState('');
-  const [word, setWord] = useState('katakroker');
-  const [userLetters, setUserLetters] = useState('');
+  const [word, setWord] = useState('');
+  const [userLetters, setUserLetters] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dev.adalab.es/api/random/word')
+      .then((response) => response.json())
+      .then((info) => {
+        setWord(info.word);
+      });
+  }, []);
+
+
+
+
 
   const handleInput = (ev) => {
     const regex = /^[a-zA-ZáéíóúüÁÉÍÓÚÜñÑ]+$/;
@@ -19,36 +31,40 @@ function App() {
       setLastLetter('');
     }
 
-    console.log(ev.target.value);
     setUserLetters([...userLetters, ev.target.value]);
   };
 
-  const handleClick = () => {
-    console.log('holi');
+  const getNumberOferrors = () => {
+    const numberErrors = userLetters.filter(
+      (pepino) => word.includes(pepino) === false
+    );
 
-    setNumberOfErrors(numberOfErrors + 1);
-    return numberOfErrors;
+    return numberErrors.length;
   };
 
   //funcions,vars,handles
 
+  //const renderNumberOfErrors =() => {
+  //numberOfErrors =  !errorLetters.filter(pepino =>)
+
   const renderErrorLetters = () => {
-    const wordLetters = word.split('');
-    const errorLetters = userLetters.filter(pepino);
-    return wordLetters.map((pepino, index) => {
-      return (
-        <li className='letter' key={index}>
-          {userLetters.includes(!pepino) ? pepino : ''}
-        </li>
-      );
-    });
+    const errorLetters = userLetters.filter((pepino) => !word.includes(pepino));
+    return (
+      <ul className="letters">
+        {errorLetters.map((pepino, index) => (
+          <li className="letter" key={index}>
+            {pepino}
+          </li>
+        ))}
+      </ul>
+    );
   };
 
   const renderSolutionLetter = () => {
     const wordLetters = word.split('');
     return wordLetters.map((pepino, index) => {
       return (
-        <li className='letter' key={index}>
+        <li className="letter" key={index}>
           {userLetters.includes(pepino) ? pepino : ''}
         </li>
       );
@@ -57,52 +73,51 @@ function App() {
 
   return (
     <>
-      <div className='page'>
+      <div className="page">
         <header>
-          <h1 className='header__title'>Juego del ahorcado</h1>
+          <h1 className="header__title">Juego del ahorcado</h1>
         </header>
-        <main className='main'>
+        <main className="main">
           <section>
-            <div className='solution'>
-              <h2 className='title'>Solución</h2>
-              <ul className='letters'>{renderSolutionLetter()}</ul>
+            <div className="solution">
+              <h2 className="title">Solución</h2>
+              <ul className="letters">{renderSolutionLetter()}</ul>
             </div>
-            <div className='error'>
-              <h2 className='title'>Letras falladas:</h2>
-              <ul className='letters'>{renderErrorLetters()}</ul>
+            <div className="error">
+              <h2 className="title">Letras falladas:</h2>
+              {renderErrorLetters()}
             </div>
-            <form className='form'>
-              <label className='title' htmlFor='last-letter'>
+            <form className="form">
+              <label className="title" htmlFor="last-letter">
                 Escribe una letra:
               </label>
               <input
-                autoComplete='off'
-                className='form__input'
-                maxLength='1'
-                type='text'
-                name='last-letter'
-                id='last-letter'
+                autoComplete="off"
+                className="form__input"
+                maxLength="1"
+                type="text"
+                name="last-letter"
+                id="last-letter"
                 value={lastLetter}
                 onChange={handleInput}
               />
             </form>
           </section>
-          <section className={'dummy error-' + numberOfErrors}>
-            <span className='error-13 eye'></span>
-            <span className='error-12 eye'></span>
-            <span className='error-11 line'></span>
-            <span className='error-10 line'></span>
-            <span className='error-9 line'></span>
-            <span className='error-8 line'></span>
-            <span className='error-7 line'></span>
-            <span className='error-6 head'></span>
-            <span className='error-5 line'></span>
-            <span className='error-4 line'></span>
-            <span className='error-3 line'></span>
-            <span className='error-2 line'></span>
-            <span className='error-1 line'></span>
+          <section className={'dummy error-' + getNumberOferrors()}>
+            <span className="error-13 eye"></span>
+            <span className="error-12 eye"></span>
+            <span className="error-11 line"></span>
+            <span className="error-10 line"></span>
+            <span className="error-9 line"></span>
+            <span className="error-8 line"></span>
+            <span className="error-7 line"></span>
+            <span className="error-6 head"></span>
+            <span className="error-5 line"></span>
+            <span className="error-4 line"></span>
+            <span className="error-3 line"></span>
+            <span className="error-2 line"></span>
+            <span className="error-1 line"></span>
           </section>
-          <button onClick={handleClick}>Incrementar</button>
         </main>
       </div>
     </>
